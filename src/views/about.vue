cbf2d524bd280fc5176a765a166bf97cdf266ad5
Shittu-Moshood <shittumoshoodademola@gmail.com>
2022-03-25 23:19:58 +0100

    new

--- src/views/about.vue
+++ src/views/about.vue
@@ -59,7 +59,6 @@ import { bus } from '../main'
  import AboutGlance from "@/components/aboutcomponents/aboutGlance"
  import AboutFooter from "@/components/aboutcomponents/aboutfooter"
  import AboutProcess from "@/components/aboutcomponents/aboutprocess"
-
 export default {
    components: {
     Carousel3d,
@@ -87,7 +86,6 @@ export default {
     mounted () {
      bus.$emit('changeClass', this.compName)
  }
-
 }
 </script>
 
@@ -98,7 +96,6 @@ export default {
   background-position: center;
   background-repeat: no-repeat;
   background-size: cover;
-
 }
 .overlayParent{
     background: linear-gradient( to right, #f2a839b4 , #f4733b7c);
@@ -114,7 +111,6 @@ export default {
 }
 .result_driven{
     margin-top: 4em;
-
 }
 .head{
     font-size: 2.9em;
@@ -127,7 +123,6 @@ export default {
  margin-top: 4em;
  margin-bottom: 0.5em;
 }
-
 /* .carousel-3d-container{
   background-color: black;
   padding: 2em 0;
@@ -138,11 +133,9 @@ export default {
  overflow: visible;
  min-height: 400px;
 } */
-
 .carousel-3d-slide{
   border: none;
   background: white;
-
 }
 .imageContents > .avatar_name{
   font-weight: bold;
@@ -157,15 +150,12 @@ export default {
    object-position: top;
    object-fit: cover;
   box-shadow: 0px 3px 10px  rgba(19, 18, 18, 0.404);
-
 }
 .carousel-3d-slide > .imageContents{
   opacity: 0;
 }
-
 .carousel-3d-slide.current > .image{
   opacity: 1;
-
 }
 .carousel-3d-slide.current > .imageContents{
   opacity: 1;
@@ -174,7 +164,6 @@ export default {
 .team_heads{
   margin-top: 8em;
    margin-bottom: 5em;
-
 }
 .team_heads > .head_meet_teams {
    font-family: montserrat, sans-serif;
@@ -250,14 +239,10 @@ export default {
     max-width: 200px;
     margin: auto 5em;
   min-height: 100%;
-
-
   }
   .carousel-3d-slide > .image{
   height: 70%;
-
   }
-
   .imageContents > .avatar_name{
     font-size: 1em;
   }
@@ -270,5 +255,4 @@ export default {
   top: -0.5em;
 }
 }
-
 </style>
